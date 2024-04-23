0208441200c736714a875135ff7f9271c6cfe555
y16ra <y16ra@users.noreply.github.com>
2024-04-23 23:41:06 +0900

    add lambda HandleRequest

--- hello/go.mod
+++ hello/go.mod
@@ -1,3 +1,5 @@
 module github.com/y16ra/deploy-lambda-go-container/hello
 
 go 1.21.1
+
+require github.com/aws/aws-lambda-go v1.47.0
--- hello/main.go
+++ hello/main.go
@@ -1,5 +1,22 @@
 package main
 
+import (
+	"context"
+
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+type HelloEvent struct {
+	Name string `json:"name"`
+}
+
+func HandleRequest(ctx context.Context, event *HelloEvent) (string, error) {
+	if event == nil {
+		return "Hello, World!", nil
+	}
+	return "Hello, " + event.Name + "!", nil
+}
+
 func main() {
-	println("Hello, World!")
+	lambda.Start(HandleRequest)
 }
